2eda8a9780371a880ab51f7ccb7d5e9350f724ef
Dang Thuyen Vuong <dangthuyenvuong@gmail.com>
2022-12-28 14:07:36 +0700

    udpate

--- src/config/path.js
+++ src/config/path.js
@@ -1,3 +1,16 @@
+const ACCOUNT_PATH = '/ca-nhan'
+
 export const PATH = {
-    home: '/'
+    home: '/',
+    product: '/san-pham',
+    productDetail: '/:slug-id:id',
+    category: '/:slug-c:id',
+    account: {
+        index: ACCOUNT_PATH,
+        address: ACCOUNT_PATH + '/dia-chi',
+        payment: ACCOUNT_PATH + '/the-thanh-toan',
+        wishlist: ACCOUNT_PATH + '/san-pham-yeu-thich',
+        order: ACCOUNT_PATH + '/don-hang',
+        historyProduct: ACCOUNT_PATH + '/san-pham-da-xem'
+    }
 }
